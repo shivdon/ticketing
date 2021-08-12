a77ba2ace687af99dcbad6d49ed71c7d8517db0d
shivdon <mehtashivansh2022@gmail.com>
2021-08-12 12:50:01 +0530

    'updates'

---
+++ client/pages/auth/signup/index.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../../hooks/use-request";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const storeDetails = async (e) => {
+    e.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={storeDetails}>
+      <h1>Sign Up </h1>
+      <div className="form-group">
+        <label htmlFor="input">Email Address</label>
+        <input
+          type="email"
+          required
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="input">Password</label>
+        <input
+          type="password"
+          required
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary mt-3" onClick={storeDetails}>
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default Signup;
